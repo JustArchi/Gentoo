91f7621d39a4c36dc5507298b771c65aa2c0e5da
Archi <JustArchi@JustArchi.net>
2022-03-12 23:27:59 +0100

    Misc

--- universal/etc/kernel/postinst.d/99-grub.sh
+++ universal/etc/kernel/postinst.d/99-grub.sh
@@ -2,6 +2,6 @@
 set -eu
 
 # Regenerate grub.cfg, only if system already has grub installed and making use of it
-if [ -f "/boot/grub/grub.cfg" ] && command -v "grub-mkconfig"; then
+if [ -f "/boot/grub/grub.cfg" ] && command -v "grub-mkconfig" >/dev/null; then
     grub-mkconfig -o "/boot/grub/grub.cfg"
 fi
