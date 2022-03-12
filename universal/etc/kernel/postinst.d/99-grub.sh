9c04e0428da1e4f7f6931a935b74bc264c26dc20
Archi <JustArchi@JustArchi.net>
2022-03-12 23:21:31 +0100

    Add 99-grub.sh

---
+++ universal/etc/kernel/postinst.d/99-grub.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -eu
+
+# Regenerate grub.cfg, only if system already has grub installed and making use of it
+if [ -f "/boot/grub/grub.cfg" ] && command -v "grub-mkconfig"; then
+    grub-mkconfig -o "/boot/grub/grub.cfg"
+fi
